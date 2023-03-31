7253895ad2e3bbf8dca74373b8363be6a4a1826b
Gellert19 <goldendemonfoxgg@outlook.com>
2023-03-31 12:24:40 +0100

    UwU

--- Program.cs
+++ Program.cs
@@ -5,12 +5,23 @@ namespace VernamCipher
     class Program
     {
         static Random Cipher = new Random();
-        
+
         static void Main()
         {
             StreamReader PlainText = new StreamReader("Plain.txt");
+            string Message = PlainText.ReadLine()!;
+            string Encoded = "";
+            int CipherKey = 0;
 
+            for (int i = 0; i < Message.Length; i++)
+            {
+                CipherKey = (int)Message[i] + GetRandomNumber(0, 256);
+                Encoded += ((char)CipherKey);
+            }
 
+            Console.WriteLine(Message);
+            Console.WriteLine(Encoded);
+            Console.ReadLine();
         }
 
         private static int GetRandomNumber(int lowerLimitValue, int upperLimitValue)
