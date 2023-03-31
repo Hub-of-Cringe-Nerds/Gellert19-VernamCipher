1486f3832575c0451e089e2099ff46776205445b
Gellert19 <goldendemonfoxgg@outlook.com>
2023-03-31 12:04:18 +0100

    heheheh

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+namespace VernamCipher
+{
+    class Program
+    {
+        static Random Cipher = new Random();
+        
+        static void Main()
+        {
+            StreamReader PlainText = new StreamReader("Plain.txt");
+
+
+        }
+
+        private static int GetRandomNumber(int lowerLimitValue, int upperLimitValue)
+        {
+            return Cipher.Next(lowerLimitValue, upperLimitValue + 1);
+        }
+        
+    }
+}
