0eb34ff9aaf1deb2810db56804aac305ee6ade36
Gellert19 <goldendemonfoxgg@outlook.com>
2023-04-01 15:06:35 +0100

    UwU

--- Program.cs
+++ Program.cs
@@ -8,7 +8,30 @@ namespace VernamCipher
 
         static void Main()
         {
-            StreamReader PlainText = new StreamReader("PlainText.txt");
+            string textfile = "", choice = "";
+
+            Console.Write("Do you wish to encode (E) a text file or decode (D): ");
+            choice = Console.ReadLine()!;
+            choice.ToUpper();
+
+            if(choice[0] == 'E')
+            {
+                Console.Write("Please enter the name of the text file that you wish to encode: ");
+                textfile = Console.ReadLine()!;
+
+                Encode(textfile);
+            }
+            else if(choice[0] == 'D')
+            {
+                Console.Write("Please enter the name of the text file that you wish to decode: ");
+                textfile = Console.ReadLine()!;
+            }
+
+        }
+
+        private static void Encode(string textfile)
+        {
+            StreamReader PlainText = new StreamReader(textfile);
             StreamWriter EncodedText = new StreamWriter("EncodedText.txt", false);
             EncodedText.AutoFlush = true;
             string Message = PlainText.ReadLine()!;
@@ -20,8 +43,13 @@ namespace VernamCipher
                 CipherKey = (int)Message[i] + GetRandomNumber(0, 256);
                 Encoded += ((char)CipherKey);
             }
-            
+
             EncodedText.WriteLine(Encoded);
+        }
+
+        private static void Decode(string textfile)
+        {
+            StreamReader PlainText = new StreamReader(textfile);
         }
 
         private static int GetRandomNumber(int lowerLimitValue, int upperLimitValue)
